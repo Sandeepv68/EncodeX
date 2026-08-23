219b9c9e32b8d5e693017dae35a96681729e97dd
Sandeep Vattapparambil <7328658+Sandeepv68@users.noreply.github.com>
2026-08-23 21:39:11 +0530

    homepage changes

--- site/.vitepress/theme/components/VersionBadge.vue
+++ site/.vitepress/theme/components/VersionBadge.vue
@@ -15,7 +15,12 @@
     <span v-if="dateText" class="vb-date">· {{ dateText }}</span>
     <span v-if="downloadsText" class="vb-dl">⬇ {{ downloadsText }}</span>
     <a class="vb-ext" :href="allReleasesUrl" target="_blank" rel="noopener noreferrer">
-      {{ t.viewOnGitHub }} ↗
+      <svg class="vb-gh" viewBox="0 0 24 24" width="15" height="15" fill="currentColor" aria-hidden="true">
+        <path
+          d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.58.11.79-.25.79-.55 0-.27-.01-1.17-.02-2.13-3.2.7-3.88-1.36-3.88-1.36-.52-1.33-1.28-1.68-1.28-1.68-1.04-.71.08-.7.08-.7 1.15.08 1.76 1.19 1.76 1.19 1.03 1.75 2.69 1.25 3.35.95.1-.74.4-1.25.72-1.54-2.55-.29-5.24-1.28-5.24-5.68 0-1.26.45-2.28 1.19-3.09-.12-.29-.52-1.46.11-3.05 0 0 .97-.31 3.18 1.18a11.05 11.05 0 0 1 5.79 0c2.21-1.49 3.18-1.18 3.18-1.18.63 1.59.23 2.76.12 3.05.74.81 1.18 1.83 1.18 3.09 0 4.41-2.69 5.38-5.26 5.67.41.36.78 1.06.78 2.14 0 1.55-.01 2.79-.01 3.17 0 .31.21.67.8.55A11.51 11.51 0 0 0 23.5 12C23.5 5.65 18.35.5 12 .5z"
+        />
+      </svg>
+      {{ t.viewOnGitHub }}
     </a>
   </p>
 </template>
@@ -182,6 +187,9 @@ const downloadsText = computed(() => {
 
 .vb-ext {
   margin-left: 12px;
+  display: inline-flex;
+  align-items: center;
+  gap: 6px;
   font-size: 12.5px;
   font-weight: 500;
   color: var(--vp-c-text-2);
